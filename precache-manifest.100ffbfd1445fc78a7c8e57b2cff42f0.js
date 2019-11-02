e67f6786bf3bd924a81e8d547e015e397ffc1a29
jin cong <jc3664@gmail.com>
2019-11-02 22:56:15 +1100

    Updates

---
+++ precache-manifest.100ffbfd1445fc78a7c8e57b2cff42f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9398c6e3f090e6bd8d19508aff80269d",
+    "url": "/solitaire/index.html"
+  },
+  {
+    "revision": "0e8682d6274b5c284410",
+    "url": "/solitaire/static/css/main.04630b00.chunk.css"
+  },
+  {
+    "revision": "0ddf156f2a06b4bd0b8c",
+    "url": "/solitaire/static/js/2.d3e2b36a.chunk.js"
+  },
+  {
+    "revision": "0e8682d6274b5c284410",
+    "url": "/solitaire/static/js/main.8fd5df5b.chunk.js"
+  },
+  {
+    "revision": "fdfde6526e33b9d9a580",
+    "url": "/solitaire/static/js/runtime-main.49a25d11.js"
+  }
+]);
